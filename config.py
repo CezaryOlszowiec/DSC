13f2368912ed037d50d6bf7bb83284dbf71f1ff6
Cezary Olszowiec <cezary.olszowiec@gmail.com>
2019-04-07 22:25:12 +0200

    First draft.

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ whitelist = set('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
 #---------------------------------------------------------------------------------------------------------
 parameters_data = {
-        'generate_data': True,
-        'how_many_files': 8000,
+        'generate_data': False,
+        'how_many_files': 100,
         'data_directory': r'../Dynamical_Systems_Classifier/data/',
         'shuffle': True, 
         'num_workers': 0,
@@ -29,22 +29,22 @@ parameters_ode = {
         'list_of_invariant_objects': ['sink', 'source', 'hyperbolic equilibrium'],
         't_start': 0,
         't_end': 1,
-        'number_of_intermediate_points': 1000,
+        'number_of_intermediate_points': 100,
         'initial_value_domain': (-1,1)
     }
 #---------------------------------------------------------------------------------------------------------
 parameters_rnn = {
-        'batch_size': 1000,
+        'batch_size': 20,
         'input_size': 2, #dimension of phase space
         'hidden_size': 4, #dimension of hidden layer
         'num_classes': 3, #same as n_categories
         'output_size': 3, # IT IS THE SAME NUMBER num_classes
         'num_layers': 3, #how many hidden layers?
-        'sequence_length': 1000, # == parameters_ode['number_of_intermediate_points'] == length of a single trajectory     
+        'sequence_length': 100, # == parameters_ode['number_of_intermediate_points'] == length of a single trajectory     
     }
 #---------------------------------------------------------------------------------------------------------
 parameters_teaching = {
         'num_workers': 0,
-        'epochs': 1000,
+        'epochs': 2,
         'learning_rate': 0.001,
     }
